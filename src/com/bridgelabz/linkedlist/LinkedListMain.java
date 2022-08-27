071d6347599030698f4cb035ed28127b57ce3bc9
sudha123 <sudhanaukala1997@gmail.com>
2022-08-27 13:47:10 +0530

    inserting

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -4,14 +4,12 @@ public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList = new LinkedList();
         linkedList.push(70);
-        linkedList.push(30);
         linkedList.push(56);
+
+        linkedList.insert(30);
+
         linkedList.display();
 
-        LinkedList<Integer> linkedList1 = new LinkedList();
-        linkedList1.append(56);
-        linkedList1.append(30);
-        linkedList1.append(70);
-        linkedList1.display();
+
     }
 }
