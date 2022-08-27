944b191bc9e815fd174d7e7bac21f58145031f9b
sudha123 <sudhanaukala1997@gmail.com>
2022-08-27 12:39:06 +0530

    addingelements

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -4,6 +4,17 @@ public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
 
+    void push(T data) {
+        Node<T> node = new Node<>(data);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            node.next = head;
+            head = node;
+        }
+    }
+
     void display() {
         Node<T> temp = head;
         while (temp != null) {
